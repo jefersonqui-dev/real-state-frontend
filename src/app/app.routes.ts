93c79b93b4c355fa7a26e60487c7864e1a7b804a
Jeferson Quiguantar <jeffersonquiguantar@gmail.com>
2025-07-24 09:36:04 -0500

    Construimos una lista con sus propiedades, menejo de Pipelines

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,10 @@
 import { Routes } from '@angular/router';
+import { PropertyList } from './features/properties/components/property-list/property-list';
 
 export const routes: Routes = [
-
+{
+    path: '',
+    component: PropertyList
+}
 
 ];

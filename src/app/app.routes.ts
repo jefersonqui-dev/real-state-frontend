b3ce2fc6c073644ea3c78e9d514fdd2d4c789fa0
Jeferson Quiguantar <jeffersonquiguantar@gmail.com>
2025-07-24 12:48:54 -0500

    Preparamos componente para implemntar signals

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,15 @@
 import { Routes } from '@angular/router';
 import { PropertyList } from './features/properties/components/property-list/property-list';
+import { Signal } from './features/samples/signal/signal';
 
 export const routes: Routes = [
 {
     path: '',
     component: PropertyList
+},
+{
+    path: 'signal',
+    component: Signal
 }
 
 ];

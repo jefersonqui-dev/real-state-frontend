5402f52f78167d2fa58082d8ec701544daf36281
Jeferson Quiguantar <jeffersonquiguantar@gmail.com>
2025-07-26 10:35:16 -0500

    implementacion de ngOnChange

--- src/app/features/samples/hooks/lifecycle.ts
+++ src/app/features/samples/hooks/lifecycle.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit} from "@angular/core";
+import { Component, Input,OnInit, OnChanges, SimpleChange, SimpleChanges} from "@angular/core";
 @Component({
     selector: 'app-lifecycle',
     template: `<p>Lifecycle works</p>`,
     
 })
-export class Lifecycle implements OnInit{
-
+export class Lifecycle implements OnInit,OnChanges{
+    @Input() name: string = '';
     constructor(){
         console.log("Creando el Componente");
     }
+    ngOnChanges(OnChanges: SimpleChanges):void{
+    console.log("Recibiendo Cambios en los Parametros");
+    }
    ngOnInit(): void{
       console.log("Inicializando el componente");
-   }
-}
+   }   
+}       
